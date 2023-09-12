19b25605e74cc4a4994fbde0e834f283887447cb
TylerSchaefer4 <93676493+TylerSchaefer4@users.noreply.github.com>
2023-09-12 11:29:33 -0400

    print message when done

--- src/index.ts
+++ src/index.ts
@@ -10,5 +10,7 @@ console.log(`result_subtract = ${result_subtract}`);
 const result_multiply = calc.multiply(3, 2);
 console.log(`result_multiply = ${result_multiply}`);
 
-const result_divide = calc.divide(6,3);
+const result_divide = calc.divide(6, 3);
 console.log(`result_divide = ${result_divide}`);
+
+console.log("done.");
